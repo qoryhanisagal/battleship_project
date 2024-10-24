c5726c64496f7c6051097d25049d5692c7d2a373
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-25 01:34:55 +0200

    Add require 'spec_helper' and comments to all spec files

---
+++ spec/cell_spec.rb
@@ -0,0 +1,6 @@
+# spec/ship_spec.rb
+# This file tests the functionality of the Cell class.
+# It requires the 'spec_helper' to load the RSpec configuration.
+require 'spec_helper'
+
+# Your tests go here...
---
+++ spec/ship_spec.rb
@@ -0,0 +1,6 @@
+# spec/ship_spec.rb
+# This file tests the functionality of the Ship class.
+# It requires the 'spec_helper' to load the RSpec configuration.
+require 'spec_helper'
+
+# Your tests go here...
