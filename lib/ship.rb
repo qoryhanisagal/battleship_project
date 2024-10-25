11f0a3ad490ed4b62261964e2795c88fe294655a
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2024-10-26 00:09:28 +0200

    Implement updates for Ship class and ship_spec.rb with JB and QORY improvements

--- lib/ship.rb
+++ lib/ship.rb
@@ -4,6 +4,7 @@ class Ship
 
   # Initializes the ship with a name and a length
   # Health is set to the length of the ship initially
+  # JB: Initial creation of the Ship class with basic attributes
   def initialize(name, length)
     @name = name
     @length = length
@@ -11,11 +12,14 @@ class Ship
   end
 
   # Reduces the health of the ship by one when hit
+  # JB: Added hit method to reduce health when the ship is hit
+  # QD: Added safeguard to prevent health from going negative
   def hit
     @health -= 1 if @health > 0
   end
 
   # Checks if the ship has sunk (health is zero)
+  # JB: Added sunk? method to check if the ship is fully damaged
   def sunk?
     @health == 0
   end
