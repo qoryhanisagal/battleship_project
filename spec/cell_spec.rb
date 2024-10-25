1f495e458a7fff0451fc3e80461497194567042e
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-25 11:47:59 -0400

    Second Cell spec passes (JB)

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -20,6 +20,12 @@ RSpec.describe Cell do
             expect(cell).to be_a Cell
         end
 
+        it 'has a coordinate' do
+            cell = Cell.new("B4")
+
+            expect(cell.coordinate).to eq "B4"
+        end
+
     end
-    
+
 end
