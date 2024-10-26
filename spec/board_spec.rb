d312240041dab33a8a6dfe523195dc5dc404db1b
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-26 14:32:40 -0400

    Update Board Spec based on iteration pattern; spec file complete but not passing (JB)

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -61,6 +61,23 @@ RSpec.describe Board do
             expect(board.valid_placement?(submarine, ["C1", "B1"])).to eq false
         end
 
+        xit 'returns false if coordinates are diagonal' do
+            board = Board.new
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2)  
+
+            expect(board.valid_placement?(cruiser, ["A1", "B2", "C3"])).to eq false
+            expect(board.valid_placement?(submarine, ["C2", "D3"])).to eq false
+        end
+
+        xit 'returns true if ship placement is valid' do
+            board = Board.new
+            cruiser = Ship.new("Cruiser", 3)
+            submarine = Ship.new("Submarine", 2) 
+            
+            expect(board.valid_placement?(submarine, ["A1", "A2"])).to eq true
+            expect(board.valid_placement?(cruiser, ["B1", "C1", "D1"])).to eq true
+        end
     end
 end
 
