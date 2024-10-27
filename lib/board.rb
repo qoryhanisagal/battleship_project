5e7cfae75a032d88e9111463f3ccca7600c76642
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-27 10:33:24 -0400

    JB: Update lib/spec with #valid_placement through length of ship/coordinates

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,6 @@
+# require './lib/cell'
+# require './lib/ship'
+
 class Board
     attr_reader :cells
 
@@ -7,8 +10,8 @@ class Board
 
     def create_cells
         grid = {}
-        ('A'..'D').each do |letter| #creates rows
-            (1..4).each do |number| #creates columns
+        ('A'..'D').each do |letter| #creates rows/x-axis
+            (1..4).each do |number| #creates columns/y-axis
                 coordinate = "#{letter}#{number}"
                 grid[coordinate] = Cell.new(coordinate)
             end
@@ -17,20 +20,28 @@ class Board
     end
 
     def valid_coordinate?(coordinate)
+        #board.cells.keys lists array of coord as strings
         @cells.key?(coordinate)
     end
 
     def valid_placement?(ship, coordinates)
         #based on length of ship
-        coordinates.length == ship.length
-
-        #based on consecutive cells
-
-
-
+        #coordinates.length == ship.length
+        if coordinates.length == ship.length &&
+            coordinates.each_cons(ship.length) do |coordinate|
+            end
+            true
+         else
+            false
+        end
+        #consecutive
+        #!diagonal
     end
 
 end
 
-require './lib/cell'
-require 'pry'; binding.pry
+# board = Board.new
+# cruiser = Ship.new("Cruiser", 3)
+# submarine = Ship.new("Submarine", 2)  
+
+# require 'pry'; binding.pry
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -47,10 +47,13 @@ RSpec.describe Board do
             submarine = Ship.new("Submarine", 2)  
 
             expect(board.valid_placement?(cruiser, ["A1", "A2"])).to eq false
+            expect(board.valid_placement?(cruiser, ["A1", "A2", "A3"])).to eq true
+
             expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq false
+            expect(board.valid_placement?(submarine, ["A2", "A3"])).to eq true
         end
 
-        it 'returns false if coordinates are not consecutive' do
+        xit 'returns false if coordinates are not consecutive' do
             board = Board.new
             cruiser = Ship.new("Cruiser", 3)
             submarine = Ship.new("Submarine", 2)  
